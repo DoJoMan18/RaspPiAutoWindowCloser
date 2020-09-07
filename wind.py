c8478ea6e11afe5cccb0cea32fe8153649ce6148
LukaUDBoogaardt <s1154960@student.windesheim.nl>
2020-09-07 11:22:59 +0200

    added wind.py

---
+++ wind.py
@@ -0,0 +1,111 @@
+from sense_hat import SenseHat
+from time import sleep
+
+sense = SenseHat()
+delay = 0.2
+
+b = (0,0,0)
+g = (0,255,0)
+r = (255,0,0)
+
+c = g
+
+
+frame1 = [
+    b,b,b,b,b,b,b,b,
+    b,b,b,b,b,b,b,b,
+    b,b,b,b,b,b,b,b,
+    b,b,b,b,b,b,c,c,
+    b,b,b,b,b,b,b,b,
+    b,b,b,b,b,b,b,b,
+    b,b,b,b,b,b,b,b,
+    b,b,b,b,b,b,c,b
+]
+
+frame2 = [
+    b,b,b,b,b,b,b,b,
+    b,b,b,b,b,b,b,b,
+    b,b,b,b,b,b,b,b,
+    b,b,b,b,c,c,c,c,
+    b,b,b,b,b,b,b,b,
+    b,b,b,b,b,b,b,b,
+    b,b,b,b,b,b,b,b,
+    b,b,b,c,c,c,c,b
+]
+
+frame3 = [
+    b,b,b,b,b,b,b,b,
+    b,b,b,b,b,b,b,b,
+    b,b,b,b,b,b,b,b,
+    b,b,b,c,c,c,c,b,
+    b,b,b,b,b,b,b,b,
+    b,b,b,b,b,b,b,b,
+    b,b,b,b,b,b,b,b,
+    b,b,c,c,c,c,b,b
+]
+
+frame3 = [
+    b,b,b,b,b,b,b,b,
+    b,c,b,b,b,b,b,b,
+    b,c,b,b,b,b,b,b,
+    b,b,c,c,c,c,b,b,
+    b,b,b,b,b,b,b,b,
+    b,b,b,b,b,b,b,b,
+    c,b,b,b,b,b,b,b,
+    b,c,c,c,c,b,b,b
+]
+
+frame4 = [
+    b,b,c,b,b,b,b,b,
+    b,c,b,c,b,b,b,b,
+    b,c,b,b,b,b,b,b,
+    b,b,c,c,b,b,b,b,
+    b,c,b,b,b,b,b,b,
+    c,b,c,b,b,b,b,b,
+    c,b,b,b,b,b,b,b,
+    b,c,c,b,b,b,b,b
+]
+
+frame5 = [
+    b,b,c,b,b,b,b,b,
+    b,c,b,c,b,b,b,b,
+    b,b,b,b,b,b,b,b,
+    b,b,b,b,b,b,b,b,
+    b,c,b,b,b,b,b,b,
+    c,b,c,b,b,b,b,b,
+    b,b,b,b,b,b,b,b,
+    b,b,b,b,b,b,b,b
+]
+
+frame6 = [
+    b,b,b,b,b,b,b,b,
+    b,b,b,c,b,b,b,b,
+    b,b,b,b,b,b,b,b,
+    b,b,b,b,b,b,b,b,
+    b,b,b,b,b,b,b,b,
+    b,b,c,b,b,b,b,b,
+    b,b,b,b,b,b,b,b,
+    b,b,b,b,b,b,b,b
+]
+
+while True:
+    sense.clear()
+    sleep(delay)
+    sense.set_pixels(frame1)
+    sleep(delay)
+    sense.set_pixels(frame2)
+    sleep(delay)
+    sense.set_pixels(frame3)
+    sleep(delay)
+    sense.set_pixels(frame4)
+    sleep(delay)
+    sense.set_pixels(frame5)
+    sleep(delay)
+    sense.set_pixels(frame6)
+#     sleep(delay)
+#     sense.set_pixels(frame7)
+#     sleep(delay)
+#     sense.set_pixels(frame8)
+    sleep(delay)
+    sense.clear()
+    sleep(delay)
