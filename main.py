e7d2e997b3b556014f6649c6d317670b137471e6
DoJoMan18 <31446154+DoJoMan18@users.noreply.github.com>
2020-09-07 11:04:54 +0200

    Reupdating main.py
    With comments

--- main.py
+++ main.py
@@ -3,21 +3,23 @@ from sense_hat import SenseHat
 from time import sleep
 import json 
 import urllib3
+
+# Setting up variables
 sh = SenseHat()
 http = urllib3.PoolManager()
 
+# API request for location
 url = 'extreme-ip-lookup.com/json/'
-#headers{'x-api-key': 'fa06f1ed94'}
-data = http.request('GET', url)
+data = http.request('GET', 'extreme-ip-lookup.com/json/')
 ipgeo = json.loads(data.data.decode('UTF-8'))
-
 location = ipgeo['city']
-url = 'weerlive.nl/api/json-data-10min.php?key=907e914202&locatie={}'.format(location)
-#headers{'x-api-key': 'fa06f1ed94'}
-data = http.request('GET', url)
-weer = json.loads(data.data.decode('UTF-8'))
+
+# API request for weather
+data = http.request(
+    'GET', 'weerlive.nl/api/json-data-10min.php?key=907e914202&locatie={}'.format(location))
+weather = json.loads(data.data.decode('UTF-8'))
+weather_humidity = weather['liveweer'][0]['lv']  # Getting humidity from weerlive
 
 
-
-weeronline_lv = weer['liveweer'][0]['lv']
-print('De luchtvochtigheid in', location, 'is', weeronline_lv)
+# Print stuff
+print('De luchtvochtigheid in', location, 'is', weather_humidity)
