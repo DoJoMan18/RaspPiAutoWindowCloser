6b84fbb03f6d7882b1e91311c17b6abcd63f9ec7
DoJoMan18 <31446154+DoJoMan18@users.noreply.github.com>
2020-09-07 11:05:32 +0200

    Stupid me

--- main.py
+++ main.py
@@ -9,14 +9,12 @@ sh = SenseHat()
 http = urllib3.PoolManager()
 
 # API request for location
-url = 'extreme-ip-lookup.com/json/'
 data = http.request('GET', 'extreme-ip-lookup.com/json/')
 ipgeo = json.loads(data.data.decode('UTF-8'))
 location = ipgeo['city']
 
 # API request for weather
-data = http.request(
-    'GET', 'weerlive.nl/api/json-data-10min.php?key=907e914202&locatie={}'.format(location))
+data = http.request('GET', 'weerlive.nl/api/json-data-10min.php?key=907e914202&locatie={}'.format(location))
 weather = json.loads(data.data.decode('UTF-8'))
 weather_humidity = weather['liveweer'][0]['lv']  # Getting humidity from weerlive
 
