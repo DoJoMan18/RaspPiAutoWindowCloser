1d96f3b758a613cd9687e9a0927237dfd1f6f21b
LukaUDBoogaardt <s1154960@student.windesheim.nl>
2020-09-07 11:59:31 +0200

    Added wind to main

--- main.py
+++ main.py
@@ -11,7 +11,114 @@ http = urllib3.PoolManager()
 reallocation = ''
 location = ''
 weather_humidity = ''
+Green = (0,255,0)
+Red = (255,0,0)
+Blue = (0,0,255)
+Yellow = (255,220,0)
+Orange = (255,130,0)
+Brown = (80,30,0)
 
+#Function to draw wind animation
+def DrawWind(color):
+    delay = 0.2
+
+    b = (0,0,0)
+
+    c = color
+
+    frame1 = [
+        b,b,b,b,b,b,b,b,
+        b,b,b,b,b,b,b,b,
+        b,b,b,b,b,b,b,b,
+        b,b,b,b,b,b,c,c,
+        b,b,b,b,b,b,b,b,
+        b,b,b,b,b,b,b,b,
+        b,b,b,b,b,b,b,b,
+        b,b,b,b,b,b,c,b
+    ]
+
+    frame2 = [
+        b,b,b,b,b,b,b,b,
+        b,b,b,b,b,b,b,b,
+        b,b,b,b,b,b,b,b,
+        b,b,b,b,c,c,c,c,
+        b,b,b,b,b,b,b,b,
+        b,b,b,b,b,b,b,b,
+        b,b,b,b,b,b,b,b,
+        b,b,b,c,c,c,c,b
+    ]
+
+    frame3 = [
+        b,b,b,b,b,b,b,b,
+        b,b,b,b,b,b,b,b,
+        b,b,b,b,b,b,b,b,
+        b,b,b,c,c,c,c,b,
+        b,b,b,b,b,b,b,b,
+        b,b,b,b,b,b,b,b,
+        b,b,b,b,b,b,b,b,
+        b,b,c,c,c,c,b,b
+    ]
+
+    frame3 = [
+        b,b,b,b,b,b,b,b,
+        b,c,b,b,b,b,b,b,
+        b,c,b,b,b,b,b,b,
+        b,b,c,c,c,c,b,b,
+        b,b,b,b,b,b,b,b,
+        b,b,b,b,b,b,b,b,
+        c,b,b,b,b,b,b,b,
+        b,c,c,c,c,b,b,b
+    ]
+
+    frame4 = [
+        b,b,c,b,b,b,b,b,
+        b,c,b,c,b,b,b,b,
+        b,c,b,b,b,b,b,b,
+        b,b,c,c,b,b,b,b,
+        b,c,b,b,b,b,b,b,
+        c,b,c,b,b,b,b,b,
+        c,b,b,b,b,b,b,b,
+        b,c,c,b,b,b,b,b
+    ]
+
+    frame5 = [
+        b,b,c,b,b,b,b,b,
+        b,c,b,c,b,b,b,b,
+        b,b,b,b,b,b,b,b,
+        b,b,b,b,b,b,b,b,
+        b,c,b,b,b,b,b,b,
+        c,b,c,b,b,b,b,b,
+        b,b,b,b,b,b,b,b,
+        b,b,b,b,b,b,b,b
+    ]
+
+    frame6 = [
+        b,b,b,b,b,b,b,b,
+        b,b,b,c,b,b,b,b,
+        b,b,b,b,b,b,b,b,
+        b,b,b,b,b,b,b,b,
+        b,b,b,b,b,b,b,b,
+        b,b,c,b,b,b,b,b,
+        b,b,b,b,b,b,b,b,
+        b,b,b,b,b,b,b,b
+    ]
+
+    sense.clear()
+    sleep(delay)
+    sense.set_pixels(frame1)
+    sleep(delay)
+    sense.set_pixels(frame2)
+    sleep(delay)
+    sense.set_pixels(frame3)
+    sleep(delay)
+    sense.set_pixels(frame4)
+    sleep(delay)
+    sense.set_pixels(frame5)
+    sleep(delay)
+    sense.set_pixels(frame6)
+    sleep(delay)
+    sense.clear()
+    sleep(delay)
 
 def getlocation():
     global location, reallocation
