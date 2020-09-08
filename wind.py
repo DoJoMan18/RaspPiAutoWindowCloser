16b779dc3656e25c1f0ef1e17e9123289e7711fe
LukaUDBoogaardt <s1154960@student.windesheim.nl>
2020-09-08 10:24:54 +0200

    Smaller wind

--- wind.py
+++ wind.py
@@ -9,14 +9,11 @@ Yellow = (255,220,0)
 Orange = (255,130,0)
 Brown = (80,30,0)
 
-def DrawWind(color):
+def DrawWind(c):
     delay = 0.2
-
     b = (0,0,0)
 
-    c = color
-
-    frame1 = [
+    frames = ([
         b,b,b,b,b,b,b,b,
         b,b,b,b,b,b,b,b,
         b,b,b,b,b,b,b,b,
@@ -25,9 +22,7 @@ def DrawWind(color):
         b,b,b,b,b,b,b,b,
         b,b,b,b,b,b,b,b,
         b,b,b,b,b,b,c,b
-    ]
-
-    frame2 = [
+    ],[
         b,b,b,b,b,b,b,b,
         b,b,b,b,b,b,b,b,
         b,b,b,b,b,b,b,b,
@@ -36,9 +31,7 @@ def DrawWind(color):
         b,b,b,b,b,b,b,b,
         b,b,b,b,b,b,b,b,
         b,b,b,c,c,c,c,b
-    ]
-
-    frame3 = [
+    ],[
         b,b,b,b,b,b,b,b,
         b,b,b,b,b,b,b,b,
         b,b,b,b,b,b,b,b,
@@ -47,9 +40,7 @@ def DrawWind(color):
         b,b,b,b,b,b,b,b,
         b,b,b,b,b,b,b,b,
         b,b,c,c,c,c,b,b
-    ]
-
-    frame3 = [
+    ],[
         b,b,b,b,b,b,b,b,
         b,c,b,b,b,b,b,b,
         b,c,b,b,b,b,b,b,
@@ -58,20 +49,7 @@ def DrawWind(color):
         b,b,b,b,b,b,b,b,
         c,b,b,b,b,b,b,b,
         b,c,c,c,c,b,b,b
-    ]
-
-    frame4 = [
-        b,b,c,b,b,b,b,b,
-        b,c,b,c,b,b,b,b,
-        b,c,b,b,b,b,b,b,
-        b,b,c,c,b,b,b,b,
-        b,c,b,b,b,b,b,b,
-        c,b,c,b,b,b,b,b,
-        c,b,b,b,b,b,b,b,
-        b,c,c,b,b,b,b,b
-    ]
-
-    frame5 = [
+    ],[
         b,b,c,b,b,b,b,b,
         b,c,b,c,b,b,b,b,
         b,b,b,b,b,b,b,b,
@@ -80,9 +58,7 @@ def DrawWind(color):
         c,b,c,b,b,b,b,b,
         b,b,b,b,b,b,b,b,
         b,b,b,b,b,b,b,b
-    ]
-
-    frame6 = [
+    ],[
         b,b,b,b,b,b,b,b,
         b,b,b,c,b,b,b,b,
         b,b,b,b,b,b,b,b,
@@ -91,23 +67,12 @@ def DrawWind(color):
         b,b,c,b,b,b,b,b,
         b,b,b,b,b,b,b,b,
         b,b,b,b,b,b,b,b
-    ]
+    ])
 
+    for i in frames:
+        sense.set_pixels(i)
+        sleep(delay)
+    
     sense.clear()
-    sleep(delay)
-    sense.set_pixels(frame1)
-    sleep(delay)
-    sense.set_pixels(frame2)
-    sleep(delay)
-    sense.set_pixels(frame3)
-    sleep(delay)
-    sense.set_pixels(frame4)
-    sleep(delay)
-    sense.set_pixels(frame5)
-    sleep(delay)
-    sense.set_pixels(frame6)
-    sleep(delay)
-    sense.clear()
-    sleep(delay)
 
 DrawWind(Blue)
